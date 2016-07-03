799a83034251195edeb7c5a47ae65d7fde07f736
tumor800 <johan_gideon@yahoo.com>
2016-07-04 01:22:25 +0700

    Change Database Config

--- db/config.inc.php
+++ db/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 //db info
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'a-database';
+$host = 'mysql.idhostinger.com';
+$username = 'u803128751_mydb';
+$password = 'lovemydb';
+$dbname = 'u803128751_mydb';
 
 // connect db
 $link = mysqli_connect($host,$username,$password,$dbname) or die('Cannot connect to server');
